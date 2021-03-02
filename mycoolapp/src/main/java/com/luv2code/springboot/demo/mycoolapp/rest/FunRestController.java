b715c39259c0c8cdedd5f65dd592ad236c2dd04a
cgokce <cagri.gokce@boun.edu.tr>
2021-03-02 22:30:25 +0300

    Index Page

---
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+
+    // Expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "HelloWorld! Time on server is " + LocalDateTime.now();
+    }
+
+}
