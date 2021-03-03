b9f68ea01b75291d8cd4803dc599e800e1874a45
cgokce <cagri.gokce@boun.edu.tr>
2021-03-03 22:13:39 +0300

    JPA DAO Implementation

--- spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.service;
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +17,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     // Dependency Injection
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
+    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO){
         employeeDAO = theEmployeeDAO;
     }
 
