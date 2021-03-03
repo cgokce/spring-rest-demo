7002a504a62afacff3df6f39b38adaa33f0a1fd0
cgokce <cagri.gokce@boun.edu.tr>
2021-03-03 22:28:12 +0300

    Spring Data JPA

--- spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,48 +1,53 @@
 package com.luv2code.springboot.cruddemo.service;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
+    private EmployeeRepository employeeRepository;
 
-    private EmployeeDAO employeeDAO;
-
-    // Dependency Injection
     @Autowired
-    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO){
-        employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository){
+        employeeRepository = theEmployeeRepository;
     }
 
     // Delegate Calls to the DAO
+    // Jpa Repository already provides the transactional, there is no need for annotation
     @Override
-    @Transactional
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+        if (result.isPresent()){
+            theEmployee = result.get();
+        } else {
+            // we did not find the employee
+            throw new RuntimeException("Did not find employee id of - " + theId);
+        }
+
+        return theEmployee;
     }
 
     @Override
-    @Transactional
     public void save(Employee theEmployee) {
-        employeeDAO.save(theEmployee);
+        employeeRepository.save(theEmployee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
+
 }
