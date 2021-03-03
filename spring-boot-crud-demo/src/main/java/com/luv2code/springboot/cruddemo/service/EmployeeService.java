7366925da5419c2640b28f8a703a8183722dd449
cgokce <cagri.gokce@boun.edu.tr>
2021-03-03 18:48:10 +0300

    Add Service Layer

--- spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
+++ spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.cruddemo.dao;
+package com.luv2code.springboot.cruddemo.service;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 import java.util.List;
 
-public interface EmployeeDAO {
+public interface EmployeeService {
 
     public List<Employee> findAll();
 
@@ -12,6 +12,6 @@ public interface EmployeeDAO {
 
     public void save(Employee theEmployee);
 
-    public  void deleteById(int theId);
+    public void deleteById(int theId);
 
 }
