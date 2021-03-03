33b94dac47cb120a759a765b8f16d8ee08bedb52
cgokce <cagri.gokce@boun.edu.tr>
2021-03-03 23:47:36 +0300

    Spring Data Rest

--- spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ spring-boot-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,8 +2,11 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // v3: Spring Data JPA
+// Spring data rest will expose /members using following
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code!
